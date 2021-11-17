d8d385854e366a5afe7eee2a18f517e620a815a3
박지영 <barkji0@naver.com>
2021-11-17 23:08:42 +0900

    start styled-commpnents

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,4 +1,4 @@
 import { makeVar } from "@apollo/client";
 
-export const isLoggedInVar = makeVar(false);
-export const dartVar = makeVar(false);
+export const isLoggedInVar = makeVar(false);  // 로그인
+export const dartVar = makeVar(false);        // 다크모드
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,16 +1,33 @@
-import { MouseEventHandler } from "react";
+import styled, { css } from "styled-components";
+import { MouseEventHandler, useState } from "react";
 import { isLoggedInVar } from "../apollo";
 
-interface ILoginProps {
-  onClick: MouseEventHandler<HTMLButtonElement>;
-}
+const Title = styled.h1`
+  color:   ${props => props.potato ? "palevioletred" : "beige"};
+  font-family: --apple-system, san-serif;
+  ${props => props.potato 
+  ? css`
+      font-size: 50px;
+    ` 
+  : css`
+      text-decoration: underline;
+    `}
+`
+const Container = styled.div`
+  background-color: tomato;
+`
+const TogglePotato = styled.button`
+  color: red;
+`
 
-function Login<ILoginProps>() {
+function Login() {
+  const [ potato, setPotato ] = useState(false);
+  const togglePotato = () => setPotato((current) => !current);
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
-    </div>
+    <Container>
+      <Title potato={ potato }>Login</Title>
+      <TogglePotato onClick={ togglePotato }>Toggle Potato</TogglePotato>
+    </Container>
   );
 }
 export default Login;
